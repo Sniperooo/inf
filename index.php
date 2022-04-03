14d3bb1f08dfa7ca29e8260473dfd503bc8d005b
Sniperooo <maciek.chorzelski@Onet.pl>
2022-04-03 18:17:05 +0200

    Main

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+	
+	include 'config.php';
+
+	if (isset($_POST['post'])) {
+
+		$nick = $_POST['nick'];
+		$msg = $_POST['msg'];
+		
+		$sql = "INSERT INTO demo (nick, msg)
+		VALUES ('$nick', '$msg')";
+
+		if ($conn->query($sql) === TRUE) {
+		  echo "";
+		} else {
+		  echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+    
+        header( "Location: {$_SERVER['PHP_SELF']}", true, 303 );
+        exit();
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+	<meta charset="UTF-8">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+	<title>Maciek Chorzelski | Komentarze</title>
+</head>
+<body>
+	<div class="wrapper">
+		<form action="" method="post" class="form">
+			<input type="text" class="nick" name="nick" placeholder="Nick">
+			<br>
+			<textarea name="msg" cols="30" rows="10" class="msg" placeholder="Komentarz"></textarea>
+			<br>
+			<button type="submit" class="btn" name="post">Napisz komentarz</button>
+		</form>
+	</div>
+
+    <div class="content">
+    <?php
+
+$sql = "SELECT * FROM demo";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+
+  while($row = $result->fetch_assoc()) {
+   
+?>
+<h3><?php echo $row['nick']; ?></h3>
+<p><?php echo $row['msg']; ?></p>
+
+<?php } } ?>
+    </div>
+</body>
+</html>
